49babf99a975e7ce6ef5911c143fbaa01dcfe822
Marcos Gerene Felix Spirito <mgf.spirito@gmail.com>
2020-05-08 09:08:10 -0300

    [*] Pacote 1.0.2 com melhorias na impressão do NFCe

--- Gerene.DFe.EscPos.Demo.NetCore/Form1.cs
+++ Gerene.DFe.EscPos.Demo.NetCore/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Drawing.Printing;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Vip.Printer.Enums;
 
